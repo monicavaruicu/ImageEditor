8dc03ee3cef44f9e5a177100f712324d72e6f102
monicavaruicu <varuicu.monica.i6x@student.ucv.ro>
2024-04-02 19:48:35 +0300

    chore: create project

---
+++ ImageEditor/ImageEditor.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace ImageEditor
+{
+    partial class ImageEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.FlipVerticalButton = new System.Windows.Forms.Button();
+            this.HorizontalFlipButton = new System.Windows.Forms.Button();
+            this.RotateButton = new System.Windows.Forms.Button();
+            this.InvertButton = new System.Windows.Forms.Button();
+            this.SepiaFilterButton = new System.Windows.Forms.Button();
+            this.BlackAndWhiteFilterButton = new System.Windows.Forms.Button();
+            this.GreenFilterButton = new System.Windows.Forms.Button();
+            this.BlueFilterButton = new System.Windows.Forms.Button();
+            this.BrightnessHighButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BrightnessLowButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ContrastHighButton = new System.Windows.Forms.Button();
+            this.ContrastLowButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(636, 379);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(122, 31);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(636, 416);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(122, 31);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.PictureBox.Location = new System.Drawing.Point(12, 12);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(590, 386);
+            this.PictureBox.TabIndex = 3;
+            this.PictureBox.TabStop = false;
+            // 
+            // FlipVerticalButton
+            // 
+            this.FlipVerticalButton.Location = new System.Drawing.Point(140, 416);
+            this.FlipVerticalButton.Name = "FlipVerticalButton";
+            this.FlipVerticalButton.Size = new System.Drawing.Size(122, 31);
+            this.FlipVerticalButton.TabIndex = 4;
+            this.FlipVerticalButton.Text = "Vertical Flip";
+            this.FlipVerticalButton.UseVisualStyleBackColor = true;
+            this.FlipVerticalButton.Click += new System.EventHandler(this.FlipVerticalButton_Click);
+            // 
+            // HorizontalFlipButton
+            // 
+            this.HorizontalFlipButton.Location = new System.Drawing.Point(12, 416);
+            this.HorizontalFlipButton.Name = "HorizontalFlipButton";
+            this.HorizontalFlipButton.Size = new System.Drawing.Size(122, 31);
+            this.HorizontalFlipButton.TabIndex = 5;
+            this.HorizontalFlipButton.Text = "Horizontal Flip";
+            this.HorizontalFlipButton.UseVisualStyleBackColor = true;
+            this.HorizontalFlipButton.Click += new System.EventHandler(this.HorizontalFlipButton_Click);
+            // 
+            // RotateButton
+            // 
+            this.RotateButton.Location = new System.Drawing.Point(268, 416);
+            this.RotateButton.Name = "RotateButton";
+            this.RotateButton.Size = new System.Drawing.Size(122, 31);
+            this.RotateButton.TabIndex = 6;
+            this.RotateButton.Text = "Rotate";
+            this.RotateButton.UseVisualStyleBackColor = true;
+            this.RotateButton.Click += new System.EventHandler(this.RotateButton_Click);
+            // 
+            // InvertButton
+            // 
+            this.InvertButton.Location = new System.Drawing.Point(396, 416);
+            this.InvertButton.Name = "InvertButton";
+            this.InvertButton.Size = new System.Drawing.Size(122, 31);
+            this.InvertButton.TabIndex = 7;
+            this.InvertButton.Text = "Invert";
+            this.InvertButton.UseVisualStyleBackColor = true;
+            this.InvertButton.Click += new System.EventHandler(this.InvertButton_Click);
+            // 
+            // SepiaFilterButton
+            // 
+            this.SepiaFilterButton.Location = new System.Drawing.Point(636, 47);
+            this.SepiaFilterButton.Name = "SepiaFilterButton";
+            this.SepiaFilterButton.Size = new System.Drawing.Size(122, 28);
+            this.SepiaFilterButton.TabIndex = 8;
+            this.SepiaFilterButton.Text = "Sepia";
+            this.SepiaFilterButton.UseVisualStyleBackColor = true;
+            this.SepiaFilterButton.Click += new System.EventHandler(this.SepiaFilterButton_Click);
+            // 
+            // BlackAndWhiteFilterButton
+            // 
+            this.BlackAndWhiteFilterButton.Location = new System.Drawing.Point(636, 12);
+            this.BlackAndWhiteFilterButton.Name = "BlackAndWhiteFilterButton";
+            this.BlackAndWhiteFilterButton.Size = new System.Drawing.Size(122, 29);
+            this.BlackAndWhiteFilterButton.TabIndex = 9;
+            this.BlackAndWhiteFilterButton.Text = "Black && White";
+            this.BlackAndWhiteFilterButton.UseVisualStyleBackColor = true;
+            this.BlackAndWhiteFilterButton.Click += new System.EventHandler(this.BlackAndWhiteFilterButton_Click);
+            // 
+            // GreenFilterButton
+            // 
+            this.GreenFilterButton.Location = new System.Drawing.Point(636, 81);
+            this.GreenFilterButton.Name = "GreenFilterButton";
+            this.GreenFilterButton.Size = new System.Drawing.Size(122, 28);
+            this.GreenFilterButton.TabIndex = 10;
+            this.GreenFilterButton.Text = "Green";
+            this.GreenFilterButton.UseVisualStyleBackColor = true;
+            this.GreenFilterButton.Click += new System.EventHandler(this.GreenFilterButton_Click);
+            // 
+            // BlueFilterButton
+            // 
+            this.BlueFilterButton.Location = new System.Drawing.Point(636, 115);
+            this.BlueFilterButton.Name = "BlueFilterButton";
+            this.BlueFilterButton.Size = new System.Drawing.Size(122, 28);
+            this.BlueFilterButton.TabIndex = 11;
+            this.BlueFilterButton.Text = "Blue";
+            this.BlueFilterButton.UseVisualStyleBackColor = true;
+            this.BlueFilterButton.Click += new System.EventHandler(this.BlueFilterButton_Click);
+            // 
+            // BrightnessHighButton
+            // 
+            this.BrightnessHighButton.Location = new System.Drawing.Point(745, 172);
+            this.BrightnessHighButton.Name = "BrightnessHighButton";
+            this.BrightnessHighButton.Size = new System.Drawing.Size(22, 28);
+            this.BrightnessHighButton.TabIndex = 12;
+            this.BrightnessHighButton.Text = "+";
+            this.BrightnessHighButton.UseVisualStyleBackColor = true;
+            this.BrightnessHighButton.Click += new System.EventHandler(this.BrightnessHighButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(660, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Brightness";
+            // 
+            // BrightnessLowButton
+            // 
+            this.BrightnessLowButton.Location = new System.Drawing.Point(623, 172);
+            this.BrightnessLowButton.Name = "BrightnessLowButton";
+            this.BrightnessLowButton.Size = new System.Drawing.Size(22, 28);
+            this.BrightnessLowButton.TabIndex = 14;
+            this.BrightnessLowButton.Text = "-";
+            this.BrightnessLowButton.UseVisualStyleBackColor = true;
+            this.BrightnessLowButton.Click += new System.EventHandler(this.BrightnessLowButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(660, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Contrast";
+            // 
+            // ContrastHighButton
+            // 
+            this.ContrastHighButton.Location = new System.Drawing.Point(745, 206);
+            this.ContrastHighButton.Name = "ContrastHighButton";
+            this.ContrastHighButton.Size = new System.Drawing.Size(22, 28);
+            this.ContrastHighButton.TabIndex = 16;
+            this.ContrastHighButton.Text = "+";
+            this.ContrastHighButton.UseVisualStyleBackColor = true;
+            this.ContrastHighButton.Click += new System.EventHandler(this.ContrastHighButton_Click);
+            // 
+            // ContrastLowButton
+            // 
+            this.ContrastLowButton.Location = new System.Drawing.Point(623, 206);
+            this.ContrastLowButton.Name = "ContrastLowButton";
+            this.ContrastLowButton.Size = new System.Drawing.Size(22, 28);
+            this.ContrastLowButton.TabIndex = 17;
+            this.ContrastLowButton.Text = "-";
+            this.ContrastLowButton.UseVisualStyleBackColor = true;
+            // 
+            // ImageEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 462);
+            this.Controls.Add(this.ContrastLowButton);
+            this.Controls.Add(this.ContrastHighButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BrightnessLowButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BrightnessHighButton);
+            this.Controls.Add(this.BlueFilterButton);
+            this.Controls.Add(this.GreenFilterButton);
+            this.Controls.Add(this.BlackAndWhiteFilterButton);
+            this.Controls.Add(this.SepiaFilterButton);
+            this.Controls.Add(this.InvertButton);
+            this.Controls.Add(this.RotateButton);
+            this.Controls.Add(this.HorizontalFlipButton);
+            this.Controls.Add(this.FlipVerticalButton);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.OpenButton);
+            this.Name = "ImageEditor";
+            this.Text = "ImageEditor";
+            this.Load += new System.EventHandler(this.ImageEditor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.Button FlipVerticalButton;
+        private System.Windows.Forms.Button HorizontalFlipButton;
+        private System.Windows.Forms.Button RotateButton;
+        private System.Windows.Forms.Button InvertButton;
+        private System.Windows.Forms.Button SepiaFilterButton;
+        private System.Windows.Forms.Button BlackAndWhiteFilterButton;
+        private System.Windows.Forms.Button GreenFilterButton;
+        private System.Windows.Forms.Button BlueFilterButton;
+        private System.Windows.Forms.Button BrightnessHighButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BrightnessLowButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ContrastHighButton;
+        private System.Windows.Forms.Button ContrastLowButton;
+    }
+}
---
+++ ImageEditor/ImageEditor.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageEditor
+{
+    public partial class ImageEditor : Form
+    {
+        public ImageEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageEditor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "Imagini|*.jpg;*.jpeg;*.png;*.bmp|Toate fișierele|*.*" };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                PictureBox.Image = Image.FromFile(openFileDialog.FileName);
+                PictureBox.Image = ScaleImage(PictureBox.Image, PictureBox.Size);
+            }
+        }
+
+        private Image ScaleImage(Image image, Size pictureBoxSize)
+        {
+            int width = image.Width;
+            int height = image.Height;
+            int newWidth, newHeight;
+
+            if ((float)width / pictureBoxSize.Width > (float)height / pictureBoxSize.Height)
+            {
+                newWidth = pictureBoxSize.Width;
+                newHeight = (int)((float)height / width * newWidth);
+            }
+            else
+            {
+                newHeight = pictureBoxSize.Height;
+                newWidth = (int)((float)width / height * newHeight);
+            }
+
+            Bitmap scaledImage = new Bitmap(newWidth, newHeight);
+            using (Graphics graphics = Graphics.FromImage(scaledImage))
+            {
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+            }
+
+            return scaledImage;
+        }
+
+        private void FlipVerticalButton_Click(object sender, EventArgs e)
+        {
+            if (PictureBox.Image != null)
+            {
+                PictureBox.Image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                PictureBox.Refresh();
+            }
+        }
+
+        private void HorizontalFlipButton_Click(object sender, EventArgs e)
+        {
+            if (PictureBox.Image != null)
+            {
+                PictureBox.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                PictureBox.Refresh();
+            }
+        }
+
+        private void RotateButton_Click(object sender, EventArgs e)
+        {
+            if (PictureBox.Image != null)
+            {
+                Bitmap bmp = new Bitmap(PictureBox.Image);
+                bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                PictureBox.Image = bmp;
+            }
+        }
+
+        private void InvertButton_Click(object sender, EventArgs e)
+        {
+            InvertImage(PictureBox.Image);
+        }
+
+        private void InvertImage(Image image)
+        {
+            if (image != null)
+            {
+                Bitmap bmp = new Bitmap(image);
+
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        Color pixelColor = bmp.GetPixel(x, y);
+                        Color invertedColor = Color.FromArgb(255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B);
+                        bmp.SetPixel(x, y, invertedColor);
+                    }
+                }
+
+                PictureBox.Image = bmp;
+            }
+        }
+
+        private Bitmap ApplyBlackAndWhiteFilter(Image image)
+        {
+            Bitmap filteredImage = new Bitmap(image);
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color pixelColor = filteredImage.GetPixel(x, y);
+                    int avgColor = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                    Color newColor = Color.FromArgb(avgColor, avgColor, avgColor);
+                    filteredImage.SetPixel(x, y, newColor);
+                }
+            }
+
+            return filteredImage;
+        }
+
+        private void BlackAndWhiteFilterButton_Click(object sender, EventArgs e)
+        {
+            if (PictureBox != null)
+            {
+                Bitmap filteredImage = ApplyBlackAndWhiteFilter(PictureBox.Image);
+                PictureBox.Image = filteredImage;
+            }
+        }
+
+        private void SepiaFilterButton_Click(object sender, EventArgs e)
+        {
+            if (PictureBox.Image != null)
+            {
+                Bitmap filteredImage = ApplySepiaFilter(PictureBox.Image);
+                PictureBox.Image = filteredImage;
+            }
+        }
+
+        private Bitmap ApplySepiaFilter(Image image)
+        {
+            Bitmap filteredImage = new Bitmap(image);
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color pixelColor = filteredImage.GetPixel(x, y);
+                    Color newColor = CalculateSepia(pixelColor);
+                    filteredImage.SetPixel(x, y, newColor);
+                }
+            }
+
+            return filteredImage;
+        }
+
+        private Color CalculateSepia(Color color)
+        {
+            int r = (int)(color.R * 0.393 + color.G * 0.769 + color.B * 0.189);
+            int g = (int)(color.R * 0.349 + color.G * 0.686 + color.B * 0.168);
+            int b = (int)(color.R * 0.272 + color.G * 0.534 + color.B * 0.131);
+
+            r = Math.Min(255, r);
+            g = Math.Min(255, g);
+            b = Math.Min(255, b);
+
+            return Color.FromArgb(r, g, b);
+        }
+
+        private void GreenFilterButton_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(PictureBox.Image);
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color originalColor = image.GetPixel(x, y);
+
+                    // Crește intensitatea culorii verzi
+                    int r = originalColor.R;
+                    int g = Math.Min(255, (int)(originalColor.G * 1.5)); // Creștere cu 50%
+                    int b = originalColor.B;
+
+                    // Actualizează pixelul din imaginea rezultat
+                    Color newColor = Color.FromArgb(r, g, b);
+                    image.SetPixel(x, y, newColor);
+                }
+            }
+
+
+            PictureBox.Image = image;
+        }
+
+        private void BlueFilterButton_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(PictureBox.Image);
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color originalColor = image.GetPixel(x, y);
+
+                    // Păstrează componenta albastră și scade celelalte componente
+                    int tr = originalColor.R / 2;
+                    int tg = originalColor.G / 2;
+                    int tb = originalColor.B;
+
+                    Color newColor = Color.FromArgb(tr, tg, tb);
+                    image.SetPixel(x, y, newColor);
+                }
+            }
+
+            PictureBox.Image = image;
+        }
+
+        private void BrightnessHighButton_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(PictureBox.Image);
+            int brightness = 5;
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color originalColor = image.GetPixel(x, y);
+                    int newR = Math.Max(0, Math.Min(255, originalColor.R + brightness));
+                    int newG = Math.Max(0, Math.Min(255, originalColor.G + brightness));
+                    int newB = Math.Max(0, Math.Min(255, originalColor.B + brightness));
+                    image.SetPixel(x, y, Color.FromArgb(newR, newG, newB));
+                }
+            }
+
+            PictureBox.Image = image;
+        }
+
+        private void BrightnessLowButton_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(PictureBox.Image);
+            int brightness = 5;
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color originalColor = image.GetPixel(x, y);
+
+                    // Reducem intensitatea tuturor componentelor culorilor
+                    int newR = Math.Max(0, originalColor.R - brightness);
+                    int newG = Math.Max(0, originalColor.G - brightness);
+                    int newB = Math.Max(0, originalColor.B - brightness);
+
+                    Color newColor = Color.FromArgb(newR, newG, newB);
+                    image.SetPixel(x, y, newColor);
+                }
+            }
+
+            PictureBox.Image = image;
+        }
+
+        private void ContrastHighButton_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(PictureBox.Image);
+
+            double averageIntensity = CalculateAverageIntensity(image);
+            int factor = 5;
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color originalColor = image.GetPixel(x, y);
+
+                    // Calculează noua intensitate a culorii pentru fiecare componentă
+                    int newR = CalculateNewIntensity(originalColor.R, averageIntensity, factor);
+                    int newG = CalculateNewIntensity(originalColor.G, averageIntensity, factor);
+                    int newB = CalculateNewIntensity(originalColor.B, averageIntensity, factor);
+
+                    Color newColor = Color.FromArgb(newR, newG, newB);
+                    image.SetPixel(x, y, newColor);
+                }
+            }
+
+            PictureBox.Image = image;
+        }
+
+        static double CalculateAverageIntensity(Bitmap image)
+        {
+            double totalIntensity = 0;
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color pixelColor = image.GetPixel(x, y);
+                    totalIntensity += (pixelColor.R + pixelColor.G + pixelColor.B) / 3.0; // Media intensității culorilor
+                }
+            }
+
+            // Calculează media intensității culorilor pe toată imaginea
+            return totalIntensity / (image.Width * image.Height);
+        }
+
+        // Funcție pentru calculul noii intensități a culorilor
+        static int CalculateNewIntensity(int originalIntensity, double averageIntensity, double factor)
+        {
+            // Formula pentru ajustarea intensității în funcție de factorul de contrast
+            return (int)(averageIntensity + factor * (originalIntensity - averageIntensity));
+        }
+    }
+}
